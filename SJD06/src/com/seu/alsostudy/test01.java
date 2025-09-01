8970e4366ef5a7a9e202326ff7833c4febb5a02b
9hh939 <2290177361@qq.com>
2025-09-01 19:15:30 +0800

    Update test01.java
    update

--- SJD06/src/com/seu/alsostudy/test01.java
+++ SJD06/src/com/seu/alsostudy/test01.java
@@ -2,7 +2,7 @@ package com.seu.alsostudy;
 
 public class test01 {
     public static void main(String[] args) {
-        System.out.println("我Tamara莱纳！");
+        System.out.println("莱纳！");
 
     }
 }
