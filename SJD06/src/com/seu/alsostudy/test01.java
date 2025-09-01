4e20bc38f7cca6d66ca8fad0ccef9fd8a9a4202a
9hh939 <2290177361@qq.com>
2025-09-01 19:16:09 +0800

    aiya3

--- SJD06/src/com/seu/alsostudy/test01.java
+++ SJD06/src/com/seu/alsostudy/test01.java
@@ -3,6 +3,7 @@ package com.seu.alsostudy;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("我Tamara莱纳！");
+        System.out.println("hhhh-");
 
     }
 }
